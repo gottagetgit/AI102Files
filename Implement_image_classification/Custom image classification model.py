9252787033ce0805595a746ffda8b452462e3bdf
Jordinthecloud <jordi.koenderink@outlook.com>
2021-04-01 17:07:40 +0200

    First Commit

---
+++ Implement_image_classification/Custom image classification model.py
@@ -0,0 +1,77 @@
+from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateBatch, ImageFileCreateEntry, Region
+from msrest.authentication import ApiKeyCredentials
+import time
+
+# Replace with valid values
+ENDPOINT = "<your API endpoint>"
+training_key = "<your training key>"
+prediction_key = "<your prediction key>"
+prediction_resource_id = "<your prediction resource id>"
+
+credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
+trainer = CustomVisionTrainingClient(ENDPOINT, credentials)
+prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
+predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
+
+publish_iteration_name = "classifyModel"
+
+credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
+trainer = CustomVisionTrainingClient(ENDPOINT, credentials)
+
+# Create a new project
+print("Creating project...")
+project = trainer.create_project("My New Project")
+
+# Make two tags in the new project
+hemlock_tag = trainer.create_tag(project.id, "Hemlock")
+cherry_tag = trainer.create_tag(project.id, "Japanese Cherry")
+
+base_image_location = "C:\\Users\\Jordi\\PycharmProjects\\AI102Files\\Implement_image_classification\\"
+
+print("Adding images...")
+
+image_list = []
+
+for image_num in range(1, 11):
+    file_name = "hemlock_{}.jpg".format(image_num)
+    with open(base_image_location + "images/Hemlock/" + file_name, "rb") as image_contents:
+        image_list.append(
+            ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[hemlock_tag.id]))
+
+for image_num in range(1, 11):
+    file_name = "japanese_cherry_{}.jpg".format(image_num)
+    with open(base_image_location + "images/Japanese Cherry/" + file_name, "rb") as image_contents:
+        image_list.append(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), tag_ids=[cherry_tag.id]))
+
+upload_result = trainer.create_images_from_files(project.id, ImageFileCreateBatch(images=image_list))
+if not upload_result.is_batch_successful:
+    print("Image batch upload failed.")
+    for image in upload_result.images:
+        print("Image status: ", image.status)
+    exit(-1)
+
+print("Training...")
+iteration = trainer.train_project(project.id)
+while iteration.status != "Completed":
+    iteration = trainer.get_iteration(project.id, iteration.id)
+    print("Training status: " + iteration.status)
+    time.sleep(1)
+
+# The iteration is now trained. Publish it to the project endpoint
+trainer.publish_iteration(project.id, iteration.id, publish_iteration_name, prediction_resource_id)
+print("Done!")
+
+# Now there is a trained endpoint that can be used to make a prediction
+prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
+predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
+
+with open(base_image_location + "images/Test/test_image.jpg", "rb") as image_contents:
+    results = predictor.detect_image(
+        project.id, publish_iteration_name, image_contents.read())
+
+    # Display the results.
+    for prediction in results.predictions:
+        print("\t" + prediction.tag_name +
+              ": {0:.2f}%".format(prediction.probability * 100))
