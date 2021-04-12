06662316e8c28a178d9b6b93e7711fad0a2db0c9
Jordinthecloud <jordi.koenderink@outlook.com>
2021-04-12 19:30:22 +0200

    First Commit

---
+++ Translate/Translate_speech_to_text.py
@@ -0,0 +1,44 @@
+import azure.cognitiveservices.speech as speechsdk
+
+speech_key, service_region = "YourSubscriptionKey", "YourServiceRegion"
+
+
+def translate_speech_to_text():
+    # Creates an instance of a speech translation config with specified subscription key and service region.
+    # Replace with your own subscription key and region identifier from here: https://aka.ms/speech/sdkregion
+    translation_config = speechsdk.translation.SpeechTranslationConfig(subscription=speech_key, region=service_region)
+
+    # Sets source and target languages.
+    # Replace with the languages of your choice, from list found here: https://aka.ms/speech/sttt-languages
+    fromLanguage = 'en-US'
+    toLanguage = 'de'
+    translation_config.speech_recognition_language = fromLanguage
+    translation_config.add_target_language(toLanguage)
+
+    # Creates a translation recognizer using and audio file as input.
+    recognizer = speechsdk.translation.TranslationRecognizer(translation_config=translation_config)
+
+    # Starts translation, and returns after a single utterance is recognized. The end of a
+    # single utterance is determined by listening for silence at the end or until a maximum of 15
+    # seconds of audio is processed. It returns the recognized text as well as the translation.
+    # Note: Since recognize_once() returns only a single utterance, it is suitable only for single
+    # shot recognition like command or query.
+    # For long-running multi-utterance recognition, use start_continuous_recognition() instead.
+    print("Say something...")
+    result = recognizer.recognize_once()
+
+    # Check the result
+    if result.reason == speechsdk.ResultReason.TranslatedSpeech:
+        print("RECOGNIZED '{}': {}".format(fromLanguage, result.text))
+        print("TRANSLATED into {}: {}".format(toLanguage, result.translations[toLanguage]))
+    elif result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print("RECOGNIZED: {} (text could not be translated)".format(result.text))
+    elif result.reason == speechsdk.ResultReason.NoMatch:
+        print("NOMATCH: Speech could not be recognized: {}".format(result.no_match_details))
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        print("CANCELED: Reason={}".format(result.cancellation_details.reason))
+        if result.cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("CANCELED: ErrorDetails={}".format(result.cancellation_details.error_details))
+
+
+translate_speech_to_text()
