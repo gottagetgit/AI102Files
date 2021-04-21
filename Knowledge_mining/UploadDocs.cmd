66bae768af649b19bcae2e654ad212dff32cf846
Jordi <jordi.koenderink@outlook.com>
2021-04-21 15:20:56 +0200

    First commit

--- Knowledge_mining/UploadDocs.cmd
+++ Knowledge_mining/UploadDocs.cmd
@@ -2,15 +2,13 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set values for your storage account
-set subscription_id=YOUR_SUBSCRIPTION_ID
-set azure_storage_account=YOUR_AZURE_STORAGE_ACCOUNT_NAME
-set azure_storage_key=YOUR_AZURE_STORAGE_KEY
+set subscription_id=b9936f95-44b4-433d-be79-5a3db7765380
+set azure_storage_account=azsademo
+set azure_storage_key=5zTVq3aPUzlgniFqjqUDxekaIlrSfWbCauOKF00EQsbTgzYqEVdLKz+QcNs5J80tBcYcKeSJN/0ov2OeHJ89AQ==
 
 
 echo Creating container...
-call az storage container create --account-name !azure_storage_account! --subscription !subscription_id! --name margies --public-access blob --auth-mode key --account-key !azure_storage_key! --output none
+call az storage container create --account-name azsademo --subscription !subscription_id! --name margies --public-access blob --auth-mode key --account-key 5zTVq3aPUzlgniFqjqUDxekaIlrSfWbCauOKF00EQsbTgzYqEVdLKz+QcNs5J80tBcYcKeSJN/0ov2OeHJ89AQ== --output none
 
 echo Uploading files...
-call az storage blob upload-batch -d margies -s data --account-name !azure_storage_account! --auth-mode key --account-key !azure_storage_key!  --output none
-
-pause
+call az storage blob upload-batch -d margies -s data --account-name azsademo --auth-mode key --account-key 5zTVq3aPUzlgniFqjqUDxekaIlrSfWbCauOKF00EQsbTgzYqEVdLKz+QcNs5J80tBcYcKeSJN/0ov2OeHJ89AQ==  --output none
