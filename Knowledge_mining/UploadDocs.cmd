bffcfa0bb60e75fdaf61fa0f998dcfb1e7e21843
Jordi <jordi.koenderink@outlook.com>
2021-04-21 16:06:43 +0200

    First commit

--- Knowledge_mining/UploadDocs.cmd
+++ Knowledge_mining/UploadDocs.cmd
@@ -2,9 +2,9 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set values for your storage account
-set subscription_id=b9936f95-44b4-433d-be79-5a3db7765380
-set azure_storage_account=azsademo
-set azure_storage_key=5zTVq3aPUzlgniFqjqUDxekaIlrSfWbCauOKF00EQsbTgzYqEVdLKz+QcNs5J80tBcYcKeSJN/0ov2OeHJ89AQ==
+set subscription_id=YourSubscriptionID
+set azure_storage_account=YourStorageAccountName
+set azure_storage_key=YourStorageKey
 
 
 echo Creating container...
