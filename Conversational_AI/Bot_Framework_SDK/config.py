9edc39a0ab7da53fd5d9f10902958e751eb85d7a
Jordi <jordi.koenderink@outlook.com>
2021-05-07 19:25:31 +0200

    First commit

--- Conversational_AI/Bot_Framework_SDK/config.py
+++ Conversational_AI/Bot_Framework_SDK/config.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 import os
 
 """ Bot Configuration """
@@ -7,5 +11,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "YourAppID")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "YourAppPassword")
